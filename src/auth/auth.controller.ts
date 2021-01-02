a7c11e8a5f2244b996f7f88a0cfb4578ad7469f6
Yusuf Baş <y.penava@hotmail.com>
2021-01-02 15:40:24 +0300

    Auth control func

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -12,4 +12,9 @@ export class AuthController {
     async signUp(@Body(ValidationPipe) authCredentialsDto:AuthCredentialsDto){
         return this.authService.signUp(authCredentialsDto);
     }
+
+    @Post('/signin')
+    async signIn(@Body() authCredentialsDto:AuthCredentialsDto){
+        return this.authService.signIn(authCredentialsDto);
+    }
 }
